a3a7b7a065753f1dd5c499086a27b61c14067296
Matthew <mbrimmer1@gmail.com>
2016-06-28 22:24:38 -0400

    More html and css

--- matthewbrimmer.js
+++ matthewbrimmer.js
@@ -13,20 +13,3 @@ $(function() {
     $('#portfolio').addClass('hide');
 	});
 });
-
-$(function() {
-  $('#box-1').click(function() {
-    $('iframe').removeClass('hide');
-    $('iframe').attr('src', '../matthewbrimmer.com/memorygame/index.html');
-    $('#box-2').append('<br><button>Close Game</button></br>');
-    $('.image').addClass('hide');
-    $('#box-2').addClass('gameButton');
-  });
-});
-$(function() {
-  $('.gameButton').click(function() {
-    $('#box-2').addClass('hide');
-    $('iframe').removeAttr('src', '../matthewbrimmer.com/memorygame/index.html');
-    $('#box-2').remove('<br><button>Close Game</button></br>');
-  });
-});
