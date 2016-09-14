8f54603477ad6036f5dfae14d7a4742903271544
Matthew <mbrimmer1@gmail.com>
2016-09-13 23:09:14 -0400

    Rebuild

--- js/matthewbrimmer.js
+++ js/matthewbrimmer.js
@@ -1,24 +1,10 @@
-$(function() {
-  $('a').click(function() {
-    $('#portfolioModal').removeClass('hide');
-    $('#portfolioModal').removeClass('in');
-  });
-// Portfolio button
-	$('.port').click(function(){
-    if ($('#portfolio').hasClass('hide')) {
-      $('#portfolio').removeClass('hide');
-    }
-	});
-  $('.homeBtn').click(function(){
-    $('#portfolio').addClass('hide');
-	});
-});
 
-$('#myAffix').affix({
-  offset: {
-    top: 100,
-    bottom: function () {
-      return (this.bottom = $('.footer').outerHeight(true));
-    }
-  }
+var $root = $('html, body');
+$(document).ready(function(){
+  $('a').click(function() {
+      $root.animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top-56
+      }, 500);
+      return false;
+  });
 });
